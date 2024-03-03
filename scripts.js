ee0ebe283c36801aae9456d29569e6baf4864713
quinnbelmar66 <107013836+quinnbelmar66@users.noreply.github.com>
2024-03-03 17:21:02 -0600

    updated scripts.js with the button code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $('.card-header').parent('.card').hover(function(){
-        $(this).children('.collapse').collapse('toggle');
-    });
-});
+function moreFunction(dotId, moreId, btnId) {
+    var dots = document.getElementById(dotId);
+    var moreText = document.getElementById(moreId);
+    var btnText = document.getElementById(btnId);
+
+    // If read less is clicked, hide and change back to read more 
+    if (dots.style.display == "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    }
+    else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display= "inline";
+    }
+}
